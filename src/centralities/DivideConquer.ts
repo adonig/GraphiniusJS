28757198776664b376e6852599c17cb8b98b9e2a
Rita Papp <v_vanellus@yahoo.com>
2018-04-25 18:22:19 +0200

    supernode preparation ready

--- src/centralities/DivideConquer.ts
+++ src/centralities/DivideConquer.ts
@@ -4,24 +4,86 @@ import * as $G from '../core/Graph';
 import * as $N from '../core/Nodes';
 import * as $E from '../core/Edges';
 import * as $P from '../search/PFS';
+import * as $SU from '../utils/structUtils';
 import * as $BH from '../datastructs/binaryHeap';
 
+//do a fake partitioning - will be replaced with a real one later
 
-function prepareSuperNode(graph: $G.IGraph, partitions: Array<Array<$N.IBaseNode>>) {
+function fakePartition(graph: $G.IGraph): {} {
+  let nodes = graph.getNodes();
+  let N = Object.keys(nodes).length;
+  let part1: Array<string> = [];
+  let part2: Array<string> = [];
+  let result = {};
+  let i = 0;
 
-//points to check: output of mincutmaxflow? can I use it now?
-//arguments of Brandes, and return type
+  //do the fake partitioning
+  for (let node in nodes) {
+    let actualNode = nodes[node];
+    if (i <= N / 2 - 1) {
+      part1.push(actualNode.getID());
+    }
+    else {
+      part2.push(actualNode.getID());
+    }
+    i++;
+  }
+  result["part1"] = part1;
+  result["part2"] = part2;
 
-//parts of this function:
-//label partition number in the nodes.features
-//set a feature frontier and set it false
-//loop through edges, check the origin of their start-end nodes
-//sort them into two dictionaries, edge ID: object characteristic tuple (sigma, dist)
-//for internode edges, add the values on the fly
+  return result;
+}
 
-//then start Brandes on the partitions, take care of touching no more than 2 frontier nodes per path!
+
+function prepareSuperNode(graph: $G.IGraph, partitions: {}) {
+  //first label each node with partition number and label as non-frontier
+  let i = 0;
+  for (let part in partitions) {
+    let array = partitions[part];
+    for (let id of array) {
+      let node: $N.IBaseNode = graph.getNodeById(id);
+      node.setFeatures({ "partition": i, "frontier": false });
+    }
+    i++;
+  }
+
+  //identify frontiers and label them in features,
+  //at the same time, sort edges as interSN and intraSN
+  let dirEdges = graph.getDirEdgesArray();
+  let undEdges = graph.getUndEdgesArray();
+  let allEdges = dirEdges;
+  for (let edge of undEdges) {
+    allEdges.push(edge);
+  }
+
+  for (let edge of allEdges) {
+    let ends = edge.getNodes();
+    let a_part = ends["a"].getFeatures().partition;
+    let b_part = ends["b"].getFeatures().partition;
+    //interSN edges soon get their characteristic tuples
+    let interSNedges = {};
+    //intraSN edges just get their partition number
+    let intraSNedges = {};
+
+    //interSN edge
+    if (a_part !== b_part) {
+      ends["a"].setFeature("frontier", true);
+      ends["b"].setFeature("frontier", true);
+      interSNedges[edge.getID()] = { "sigma": 1, "dist": edge.getWeight() };
+    }
+    //intraSN edge
+    else {
+      //no need to change the endnodes (frontier:false is set already)
+      intraSNedges[edge.getID()] = ends["a"].getFeatures().partition;
+    }
+  }
+
+  //basically, we are ready to start the Brandes
+  //to note: only max 2 frontier nodes per path!
 
 
 
 
 }
+
+export { fakePartition, prepareSuperNode }
--- test/centralities/DivideAndConquerTests.ts
+++ test/centralities/DivideAndConquerTests.ts
@@ -2,6 +2,7 @@
 
 import * as fs from 'fs';
 import * as chai from 'chai';
+import * as $DC from '../../src/centralities/DivideConquer';
 import * as $G from '../../src/core/Graph';
 import * as $N from '../../src/core/Nodes';
 import * as $E from '../../src/core/Edges';
@@ -11,23 +12,33 @@ import * as $CSV from '../../src/io/input/CSVInput';
 import * as $JSON from '../../src/io/input/JSONInput';
 
 
-import {Logger} from '../../src/utils/logger';
+import { Logger } from '../../src/utils/logger';
 const logger = new Logger();
 
 let expect = chai.expect,
-    csv: $CSV.ICSVInput = new $CSV.CSVInput(" ", false, false),
-    json: $JSON.IJSONInput = new $JSON.JSONInput(true, false, true);
+  csv: $CSV.ICSVInput = new $CSV.CSVInput(" ", false, false),
+  json: $JSON.IJSONInput = new $JSON.JSONInput(true, false, true);
 
 const PATH_PREFIX = "./test/test_data/";
 let path_midSizeGraph = PATH_PREFIX + "bernd_ares_intermediate_pos.json";
+let path_search_pos = PATH_PREFIX + "search_graph_multiple_SPs_positive.json";
 let graph_midSizeGraph = json.readFromJSONFile(path_midSizeGraph);
+let graph_search_pos = json.readFromJSONFile(path_search_pos);
 
 describe('test divide and conquer', () => {
 
-  it('first test', () => {
-    
-});
-
+  it.only('label partition and frontier-boolean in each nodes features', () => {
+    let graph = graph_midSizeGraph;
+    let parts = $DC.fakePartition(graph);
+    $DC.prepareSuperNode(graph, parts);
+    let nodes = graph.getNodes();
+    for (let node in nodes){
+      console.log("nodeID: "+nodes[node].getID());
+      console.log("partition nr: "+nodes[node].getFeature("partition"));
+      console.log("frontier node: "+nodes[node].getFeature("frontier"));
+      console.log();
+    }
+  });
 
 
 });
